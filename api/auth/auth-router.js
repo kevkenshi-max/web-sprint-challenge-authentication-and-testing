cc9c95b2411fa744ce00269d59f376575afed202
Kevin Lee <kevin-lee@lambdastudents.comgit config --global user.email kevkenshi@gmail.com>
2022-03-31 22:46:36 -0700

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,8 +1,30 @@
-const router = require('express').Router();
+const Users = require("../users-model");
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../secrets");
+const {
+  checkReqBody,
+  checkUsernameFree,
+  checkUsernameExist,
+} = require("../middleware/restricted");
 
-router.post('/register', (req, res) => {
-  res.end('implement register, please!');
-  /*
+
+router.post(
+  "/register",
+  checkReqBody,
+  checkUsernameFree,
+  (req, res, next) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 8);
+    user.password = hash;
+
+    Users.add(user)
+      .then((user) => {
+        res.status(201).json(user);
+      })
+      .catch(next);
+    /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
     DO NOT EXCEED 2^8 ROUNDS OF HASHING!
@@ -27,11 +49,25 @@ router.post('/register', (req, res) => {
     4- On FAILED registration due to the `username` being taken,
       the response body should include a string exactly as follows: "username taken".
   */
-});
+  }
+);
 
-router.post('/login', (req, res) => {
-  res.end('implement login, please!');
-  /*
+router.post(
+  "/login",
+  checkReqBody,
+  checkUsernameExist,
+  (req, res, next) => {
+    const { password } = req.body;
+    if (bcrypt.compareSync(password, req.user.password)) {
+      const token = buildToken(req.user);
+      res.status(200).json({
+        message: `welcome, ${req.user.username}`,
+        token,
+      });
+    } else {
+      next();
+    }
+    /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
 
@@ -54,6 +90,18 @@ router.post('/login', (req, res) => {
     4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
       the response body should include a string exactly as follows: "invalid credentials".
   */
-});
+  }
+);
+
+function buildToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+  const options = {
+    expiresIn: "1d",
+  };
+  return jwt.sign(payload, JWT_SECRET, options);
+}
 
 module.exports = router;
--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,6 +1,7 @@
-module.exports = (req, res, next) => {
-  next();
-  /*
+const Users = require("../users-model");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../secrets");
+/*
     IMPLEMENT
 
     1- On valid token in the Authorization header, call next.
@@ -11,4 +12,68 @@ module.exports = (req, res, next) => {
     3- On invalid or expired token in the Authorization header,
       the response body should include a string exactly as follows: "token invalid".
   */
+
+const restrict = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return next({ status: 401, message: "Token required" });
+  }
+
+  jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+    if (err) {
+      return next({ status: 401, message: "Token invalid" });
+    }
+    req.decodedToken - decodedToken;
+  });
+  return next();
+};
+
+const checkReqBody = (req, res, next) => {
+  const { username, password } = req.body;
+  if (
+    !username ||
+    username.trim() === "" ||
+    typeof username !== "string" ||
+    !password
+  ) {
+    return next({ status: 400, message: "username and password required" });
+  }
+  next();
+};
+
+const checkUsernameFree = async (req, res, next) => {
+  try {
+    const { username } = req.body;
+    const existing = await Users.findBy({ username }).first();
+    if (existing) {
+      res.status(422).json({ status: 422, message: "username taken" });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+const checkUsernameExist = async (req, res, next) => {
+  try {
+    const { username } = req.body;
+    const existing = await Users.findBy({ username }).first();
+    if (!existing) {
+      res.status(401).json({ message: "invalid credentials" });
+    } else {
+      req.user = existing;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  restrict,
+  checkReqBody,
+  checkUsernameFree,
+  checkUsernameExist,
 };
